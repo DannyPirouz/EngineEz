807cf7c00ef3f6fd08d47d2652111ba3248a68c3
Danny Pirouz <danny28@student.ubc.ca>
2025-01-23 23:09:31 -0800

    added sample MyCars page

--- engine-ez/src/App.js
+++ engine-ez/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MyCars from './pages/MyCars';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path = "/" element = {<HomePage />} />
+        <Route path = "/MyCars" element = {<MyCars />} />
       </Routes>
     </Router>
   );
---
+++ engine-ez/src/pages/MyCars.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MyCars() {
+  return (
+    <div>
+      <h1>These are all of your cars</h1>
+    </div>
+  );
+}
+
+export default MyCars;
