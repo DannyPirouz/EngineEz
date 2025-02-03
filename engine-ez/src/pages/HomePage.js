d363ad53300c4d2bf0c43ba6fe0d6f00f9da5db2
Danny Pirouz <danny28@student.ubc.ca>
2025-02-02 22:48:43 -0800

    starting login display

--- engine-ez/src/pages/HomePage.js
+++ engine-ez/src/pages/HomePage.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import LoginForm from '../Components/LoginForm/LoginForm';
 
 function HomePage() {
   return (
     <div>
-      <h1>Engine Ez</h1>
+      <LoginForm />
     </div>
   );
 }
 
 // good video for creating login functionality
 // https://www.youtube.com/watch?v=STdW1117dbE
+
+// good vid for authentication
+// https://www.youtube.com/watch?v=wr3VmbZdVA4
+// https://www.youtube.com/watch?v=X3qyxo_UTR4
 
 export default HomePage;
